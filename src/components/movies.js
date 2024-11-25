9d385895905882844fa48f97b6af207ae3c051f2
Doyler3340 <72506331+Doyler3340@users.noreply.github.com>
2024-11-25 22:00:41 +0000

    Update movies.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,11 +1,20 @@
+// Importing the MovieItem component
 import MovieItem from "./movieitem";
 
-const Movies = (props)=>{
+// Functional component to render a list of movies
+const Movies = (props) => {
+    // Mapping over the array of movies passed as a prop
+    // For each movie, return a MovieItem component
     return props.myMovies.map(
-        (movie)=>{
-            return <MovieItem mymovie={movie} key={movie._id} Reload={props.ReloadData}/>
+        (movie) => {
+            // Each MovieItem receives:
+            // 1. The movie object as 'mymovie' prop
+            // 2. A unique 'key' prop using the movie's ID
+            // 3. The Reload function as 'Reload' prop for refreshing the list
+            return <MovieItem mymovie={movie} key={movie._id} Reload={props.ReloadData} />;
         }
     );
 }
 
+// Exporting the Movies component for use in other parts of the application
 export default Movies;
